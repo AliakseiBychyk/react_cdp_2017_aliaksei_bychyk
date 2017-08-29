0f3fc3eb235e5102cbeb1aad00841f63c284e3dd
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-08-29 17:01:16 +0300

    express server

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-import express from 'express'
+import config from './config';
+import express from 'express';
+import apiRouter from './api';
 
-const server = express()
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+  res.send('Hello Express');
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+
+server.listen(config.port, () => {
+  console.info('Express listening on port ', config.port);
+});
