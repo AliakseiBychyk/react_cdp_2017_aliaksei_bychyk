ce2ad22d3fb9508fbb7b6263999de9363557cda3
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-08-29 18:09:22 +0300

    next commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <h2>Hello React asfaf fafadf with JSX</h2>
+  );
+};
+ 
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
