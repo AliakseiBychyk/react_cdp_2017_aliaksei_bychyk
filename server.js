9014e5aa1d559ceb94c6f9b9156b73a68e80d3e5
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2017-09-02 13:01:02 +0300

    some improvement

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+import config from './config'
 import express from 'express'
 
 const server = express()
+
+server.get('/', (req, res) => {
+  res.send('Hello Express!')
+})
+
+server.get('/about.html', (req, res) => {
+  res.send('the about page')
+})
+
+server.listen(config.port, () => {
+  console.info('Server listening on port ', config.port)
+})
